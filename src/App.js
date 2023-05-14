129c5ca63aecaa4291c35db01ba1c689e5582d41
Roodjino Cherilus <cherilusjcr@gmail.com>
2023-05-13 21:09:18 -0400

    include routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import QuoteDisplay from './components/Quotes';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-      <QuoteDisplay />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/calculator">Calculator</Link>
+            </li>
+            <li>
+              <Link to="/quote">Quote</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/calculator">
+            <Calculator />
+          </Route>
+          <Route path="/quote">
+            <QuoteDisplay />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
 export default App;
